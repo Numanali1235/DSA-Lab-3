faeb71c95638a19fb418f84b95ddb070296ff850
Numanali1235 <na4299803@gmail.com>
2025-03-25 22:02:11 +0500

    Update Task 1.cpp

--- Task 1.cpp
+++ Task 1.cpp
@@ -9,29 +9,25 @@ protected:
 	int currentSize;
 
 public:
-	// Parameterized constructor with default arguments
 	List(int size = 10) {
 		maxSize = size;
 		currentSize = 0;
 		arr = new Type[maxSize];
 	}
 
-	// Copy constructor
 	List(const List<Type>& other) {
 		maxSize = other.maxSize;
 		currentSize = other.currentSize;
 		arr = new Type[maxSize];
-		for (int i = 0; i < currentSize; ++i) {
-			arr[i] = other.arr[i]; // Copy elements
+		for (int i = 0; i < currentSize; i++) {
+			arr[i] = other.arr[i];
 		}
 	}
 
-	// Destructor
 	virtual ~List() {
-		delete[] arr; // Free allocated memory
+		delete[] arr;
 	}
 
-	// Pure virtual functions
 	virtual void addElementAtFirstIndex(Type element) = 0;
 	virtual void addElementAtLastIndex(Type element) = 0;
 	virtual void removeElementAtFirstIndex() = 0;
@@ -42,68 +38,58 @@ public:
 template <typename Type>
 class Temp : public List<Type> {
 public:
-	// Constructor
 	Temp(int a) : List<Type>(a) {}
 
-	// Add element at the first index
 	void addElementAtFirstIndex(Type element) {
 		if (this->currentSize < this->maxSize) {
-			// Shift elements to the right
-			for (int i = this->currentSize; i > 0; --i) {
+			for (int i = this->currentSize; i > 0; i--) {
 				this->arr[i] = this->arr[i - 1];
 			}
 			this->arr[0] = element;
-			++this->currentSize; // Increment size after adding
+			this->currentSize++;
 		}
 		else {
 			cout << "List is full!" << endl;
 		}
 	}
 
-	// Add element at the last index
 	void addElementAtLastIndex(Type element) {
 		if (this->currentSize < this->maxSize) {
 			this->arr[this->currentSize] = element;
-			++this->currentSize; // Increment size after adding
+			this->currentSize++;
 		}
 		else {
 			cout << "List is full!" << endl;
 		}
 	}
 
-	// Remove the element at the first index
 	void removeElementAtFirstIndex() {
 		if (this->currentSize > 0) {
-			// Shift elements to the left
-			for (int i = 0; i < this->currentSize - 1; ++i) {
+			for (int i = 0; i < this->currentSize - 1; i++) {
 				this->arr[i] = this->arr[i + 1];
 			}
-			this->arr[this->currentSize - 1] = 0;
-			--this->currentSize;
+			this->currentSize--;
 		}
 		else {
 			cout << "List is empty!" << endl;
 		}
 	}
 
-	// Remove the element at the last index
 	void removeElementAtLastIndex() {
 		if (this->currentSize > 0) {
-			this->arr[this->currentSize - 1] = 0;
-			--this->currentSize;
+			this->currentSize--;
 		}
 		else {
 			cout << "List is empty!" << endl;
 		}
 	}
 
-	// Display the elements of the list
 	void display() const override {
 		if (this->currentSize == 0) {
 			cout << "List is empty!" << endl;
 		}
 		else {
-			for (int i = 0; i < this->currentSize; ++i) {
+			for (int i = 0; i < this->currentSize; i++) {
 				cout << this->arr[i] << " ";
 			}
 			cout << endl;
@@ -112,7 +98,6 @@ public:
 };
 
 int main() {
-	// Create a Temp object with a list size of 5
 	Temp<int> myList(5);
 
 	myList.addElementAtFirstIndex(10);
@@ -125,6 +110,8 @@ int main() {
 
 	myList.removeElementAtLastIndex();
 	myList.display();
-	system("pause");
+
+	cout << "Press Enter to continue...";
+	cin.get();
 	return 0;
 }
